684d547a8442b4bedd0d2fbe74e302df41a0d2c5
Deheng <dehengxu@outlook.com>
2014-05-04 18:33:36 +0800

    Initial Commit

---
+++ MixedCppTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MixedCppTest
+//
+//  Created by DehengXu on 14-5-4.
+//  Copyright (c) 2014年 Deheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MixedCppTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MixedCppTest
+//
+//  Created by DehengXu on 14-5-4.
+//  Copyright (c) 2014年 Deheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
